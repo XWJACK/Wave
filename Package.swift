4d64562d4782910d09ac0fcdf5e3e0a4af0af217
Jack <xuwenjiejack@gmail.com>
2017-04-28 10:49:16 +0800

    Add Swift Package

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  Wave
+//
+//  Created by Jack on 4/28/17.
+//
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Wave",
+    dependencies : [],
+    exclude: ["Demo"]
+)
